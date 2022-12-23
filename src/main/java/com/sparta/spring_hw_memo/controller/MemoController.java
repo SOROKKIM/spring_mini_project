faf49b0eaf379e787b464f2e518a0ba16f7e0756
SOROKKIM <ksl7593@gmail.com>
2022-12-24 01:20:23 +0900

    댓글 작성, 수정, 삭제

--- src/main/java/com/sparta/spring_hw_memo/controller/MemoController.java
+++ src/main/java/com/sparta/spring_hw_memo/controller/MemoController.java
@@ -1,7 +1,6 @@
 package com.sparta.spring_hw_memo.controller;
 
-import com.sparta.spring_hw_memo.dto.MemoRequestDto;
-import com.sparta.spring_hw_memo.dto.MemoResponseDto;
+import com.sparta.spring_hw_memo.dto.*;
 import com.sparta.spring_hw_memo.entity.Memo;
 import com.sparta.spring_hw_memo.service.MemoService;
 import com.sparta.spring_hw_memo.repository.MemoRepository;
@@ -23,8 +22,8 @@ public class MemoController {
     private final MemoService memoService;
     private final MemoRepository memoRepository;
 
-    @ResponseBody
     // 메모 등록하기
+    @ResponseBody
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto, HttpServletRequest request) {
 
@@ -34,26 +33,52 @@ public class MemoController {
 
     //메모 조회하기
     @GetMapping("/memos")
+    @ResponseBody
     public List<MemoResponseDto> getMemos() {
 
         return memoService.getMemos();
     }
-    @GetMapping("/memos/{id}")
-    public Optional<Memo> getMemo(@PathVariable Long id) {
+
+    //메모 상세 조회
+    @GetMapping("/memos/detail")
+    @ResponseBody
+    public MemoResponseDto getMemo(@RequestParam Long id) {
 
         return memoService.getMemo(id);
     }
 
-    //메모 업데이트
+    //메모 수정
     @PutMapping("/memos/{id}")
+    @ResponseBody
     public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto, HttpServletRequest request) {
         return memoService.update(id, requestDto, request);
     }
 
     //메모 삭제
     @DeleteMapping("/memos/{id}")
+    @ResponseBody
     public Long deleteMemo(@PathVariable Long id, HttpServletRequest request) {
         return memoService.deleteMemo(id, request);
+    }
+
+
+    // 댓글 작성
+    @PostMapping("/comment/{id}")
+    public CommentResponseDto createComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
+        return memoService.createComment(id, requestDto, request);
+    }
+
+    // 댓글 수정
+    @PutMapping("/comment/{comment_id}")
+    public CommentResponseDto updateComment(@PathVariable Long comment_id, @RequestBody CommentRequestDto requestDto, HttpServletRequest request) {
+        return memoService.updateComment(comment_id, requestDto, request);
+    }
+
+
+    // 댓글 삭제
+    @DeleteMapping("/comment/{comment_id}")
+    public ResponseDto deleteComment(@PathVariable Long comment_id, HttpServletRequest request) {
+        return memoService.deleteComment(comment_id, request);
     }
 }
 
