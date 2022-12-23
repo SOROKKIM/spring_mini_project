2c000abac3f4395b930d54ed772c959fcb3b265a
SOROKKIM <ksl7593@gmail.com>
2022-12-24 01:20:48 +0900

    댓글 RequestDto

---
+++ src/main/java/com/sparta/spring_hw_memo/dto/CommentRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.spring_hw_memo.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CommentRequestDto {
+    private String comment;
+}
